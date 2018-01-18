e969c413a1b079c913b45964ebdd0529be56b9fd
Ricardo Martins <rasm@oceanscan-mst.com>
2018-01-18 13:44:18 +0000

    Cleanup.

--- src/main/java/pt/lsts/imc/def/ValueDescriptor.java
+++ src/main/java/pt/lsts/imc/def/ValueDescriptor.java
@@ -1,9 +1,9 @@
 /*
  * Below is the copyright agreement for IMCJava.
- * 
+ *
  * Copyright (c) 2010-2016, Laboratório de Sistemas e Tecnologia Subaquática
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     - Redistributions of source code must retain the above copyright
@@ -11,143 +11,68 @@
  *     - Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     - Neither the names of IMC, LSTS, IMCJava nor the names of its 
- *       contributors may be used to endorse or promote products derived from 
+ *     - Neither the names of IMC, LSTS, IMCJava nor the names of its
+ *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
  * DISCLAIMED. IN NO EVENT SHALL LABORATORIO DE SISTEMAS E TECNOLOGIA SUBAQUATICA
  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 package pt.lsts.imc.def;
 
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
 public class ValueDescriptor {
+    private String name, abbrev, prefix;
+    private LinkedHashMap<Long, String> values = new LinkedHashMap<>();
 
-	private String name, abbrev, prefix;
-	private LinkedHashMap<Long, String> values = new LinkedHashMap<Long, String>();
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getAbbrev() {
+        return abbrev;
+    }
 
-	/**
-	 * @return the abbrev
-	 */
-	public String getAbbrev() {
-		return abbrev;
-	}
+    public void setAbbrev(String abbrev) {
+        this.abbrev = abbrev;
+    }
 
-	/**
-	 * @param abbrev
-	 *            the abbrev to set
-	 */
-	public void setAbbrev(String abbrev) {
-		this.abbrev = abbrev;
-	}
+    public String getPrefix() {
+        return prefix;
+    }
 
-	/**
-	 * @return the prefix
-	 */
-	public String getPrefix() {
-		return prefix;
-	}
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
 
-	/**
-	 * @param prefix
-	 *            the prefix to set
-	 */
-	public void setPrefix(String prefix) {
-		this.prefix = prefix;
-	}
+    public LinkedHashMap<Long, String> getValues() {
+        return values;
+    }
 
-	/**
-	 * @return the values
-	 */
-	public LinkedHashMap<Long, String> getValues() {
-		return values;
-	}
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{\n");
 
-	public static class ValueType {
-		int value;
-		String name, abbrev;
+        for (Entry<Long, String> entry : getValues().entrySet()) {
+            sb.append("\t").append(entry.getKey()).append(" -> \t").append(entry.getValue()).append("\n");
+        }
 
-		/**
-		 * @return the value
-		 */
-		public int getValue() {
-			return value;
-		}
-
-		/**
-		 * @param value
-		 *            the value to set
-		 */
-		public void setValue(int value) {
-			this.value = value;
-		}
-
-		/**
-		 * @return the name
-		 */
-		public String getName() {
-			return name;
-		}
-
-		/**
-		 * @param name
-		 *            the name to set
-		 */
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		/**
-		 * @return the abbrev
-		 */
-		public String getAbbrev() {
-			return abbrev;
-		}
-
-		/**
-		 * @param abbrev
-		 *            the abbrev to set
-		 */
-		public void setAbbrev(String abbrev) {
-			this.abbrev = abbrev;
-		}
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder("{\n");
-		
-		for (Entry<Long, String> entry : getValues().entrySet()) {
-			sb.append("\t"+entry.getKey()+" -> \t"+entry.getValue()+"\n");
-		}
-		
-		sb.append("}");
-		return sb.toString();
-	}
-
+        sb.append("}");
+        return sb.toString();
+    }
 }
