5e7314111178e16ce6bde71431a2e50e32fe96aa
Ricardo Martins <rasm@oceanscan-mst.com>
2018-01-18 12:40:12 +0000

    Cleanup.

--- src/main/java/pt/lsts/neptus/messages/listener/ImcConsumer.java
+++ src/main/java/pt/lsts/neptus/messages/listener/ImcConsumer.java
@@ -1,9 +1,9 @@
 /*
  * Below is the copyright agreement for IMCJava.
- * 
+ *
  * Copyright (c) 2010-2016, Laboratório de Sistemas e Tecnologia Subaquática
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     - Redistributions of source code must retain the above copyright
@@ -11,19 +11,19 @@
  *     - Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     - Neither the names of IMC, LSTS, IMCJava nor the names of its 
- *       contributors may be used to endorse or promote products derived from 
+ *     - Neither the names of IMC, LSTS, IMCJava nor the names of its
+ *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
  * DISCLAIMED. IN NO EVENT SHALL LABORATORIO DE SISTEMAS E TECNOLOGIA SUBAQUATICA
  * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package pt.lsts.neptus.messages.listener;
@@ -41,18 +41,17 @@ import pt.lsts.imc.IMCMessage;
 import pt.lsts.imc.net.Consume;
 
 public class ImcConsumer implements MessageListener<MessageInfo, IMCMessage> {
+	private LinkedHashMap<Class<?>, ArrayList<Method>> consumeMethods = new LinkedHashMap<>();
+	private LinkedHashMap<Method, List<String>> sources = new LinkedHashMap<>();
+	private LinkedHashMap<Method, List<String>> entities = new LinkedHashMap<>();
 
-	private LinkedHashMap<Class<?>, ArrayList<Method>> consumeMethods = new LinkedHashMap<Class<?>, ArrayList<Method>>();
-	private LinkedHashMap<Method, List<String>> sources = new LinkedHashMap<Method, List<String>>();
-	private LinkedHashMap<Method, List<String>> entities = new LinkedHashMap<Method, List<String>>();
-	
 	private Object pojo;
 
 	private ImcConsumer(Object pojo) {
 		this.pojo = pojo;
 		for (Method m : pojo.getClass().getDeclaredMethods()) {
 			if (m.getAnnotation(Consume.class) != null) {
-				
+
 				if (m.getParameterTypes().length != 1) {
 					System.err
 							.println("Warning: Ignoring @Consume annotation on method "
@@ -70,29 +69,29 @@ public class ImcConsumer implements MessageListener<MessageInfo, IMCMessage> {
 				Class<?> c = m.getParameterTypes()[0];
 
 				if (!consumeMethods.containsKey(c)) {
-					consumeMethods.put(c, new ArrayList<Method>());
+					consumeMethods.put(c, new ArrayList<>());
 				}
 				if (!m.isAccessible())
 					m.setAccessible(true);
-				
-				
+
+
 				consumeMethods.get(c).add(m);
-				
+
 				Consume annotation = m.getAnnotation(Consume.class);
-				
+
 				List<String> srcs = Arrays.asList(annotation.Source());
 				if (srcs.size() != 1 || !srcs.get(0).isEmpty())
 					sources.put(m, srcs);
 				List<String> ents = Arrays.asList(annotation.Entity());
 				if (ents.size() != 1 || !ents.get(0).isEmpty())
 					entities.put(m, ents);
-				
+
 			}
 		}
 	}
 
 	public Collection<String> getTypesToListen() {
-		HashSet<String> types = new HashSet<String>();
+		HashSet<String> types = new HashSet<>();
 		for (Class<?> c : consumeMethods.keySet()) {
 			if (c.getClass().equals(IMCMessage.class))
 				return null;
@@ -103,17 +102,17 @@ public class ImcConsumer implements MessageListener<MessageInfo, IMCMessage> {
 				types.addAll(IMCDefinition.getInstance().subtypesOf(name));
 			types.add(name);
 		}
-		
+
 		if (types.contains("IMCMessage"))
 			return null;
-		
+
 		return types;
 	}
 
 	public void onMessage(MessageInfo i, IMCMessage m) {
 		m.setMessageInfo(i);
 		Class<?> c = m.getClass();
-		ArrayList<Method> consumers = new ArrayList<Method>();
+		ArrayList<Method> consumers = new ArrayList<>();
 
 		while (c != Object.class) {
 			if (consumeMethods.containsKey(c))
