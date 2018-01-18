1e503cdb0c682665fc1505cfa0ca36e0ccdc38db
Ricardo Martins <rasm@oceanscan-mst.com>
2018-01-18 13:44:10 +0000

    Cleanup.

--- src/main/java/pt/lsts/imc/llf/LLFMessageLogger.java
+++ src/main/java/pt/lsts/imc/llf/LLFMessageLogger.java
@@ -28,7 +28,12 @@
  *
  * $Id:: LLFMessageLogger.java 333 2013-01-02 11:11:44Z zepinto                $:
  */
+
 package pt.lsts.imc.llf;
+
+import pt.lsts.imc.IMCDefinition;
+import pt.lsts.imc.IMCFieldType;
+import pt.lsts.imc.IMCMessage;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -38,137 +43,128 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Vector;
 
-import pt.lsts.imc.IMCDefinition;
-import pt.lsts.imc.IMCFieldType;
-import pt.lsts.imc.IMCMessage;
-
 public class LLFMessageLogger {
+    private LinkedHashMap<Integer, BufferedWriter> writers = new LinkedHashMap<>();
+    private File dir;
+    private double startTime = -1;
+    private String[] headerNames = new String[]{"timestamp", "src", "src_ent", "dst", "dst_ent"};
+    private String[] headerFields = null;
+    protected IMCDefinition defs = IMCDefinition.getInstance();
 
-	protected LinkedHashMap<Integer, BufferedWriter> writers = new LinkedHashMap<Integer, BufferedWriter>();
-	protected File dir;
-	protected double startTime = -1;
+    public LLFMessageLogger(String directory) {
+        dir = new File(directory);
+    }
 
-	protected String[] headerNames = new String[] {"timestamp", "src", "src_ent", "dst", "dst_ent"};
-	protected String[] headerFields = null;//new String[] {"time", "src", "src_ent", "dst", "dst_ent"};
-	protected IMCDefinition defs = IMCDefinition.getInstance();
-
-	public LLFMessageLogger(String directory) {
-		dir = new File(directory);
-	}
-
-	public void flushLogs() {
-	    for (BufferedWriter bw : writers.values()) {
-	        try {
-	            bw.flush();
-	        }
-	        catch (Exception e) {
-	            e.printStackTrace();
+    public void flushLogs() {
+        for (BufferedWriter bw : writers.values()) {
+            try {
+                bw.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-	    }
-	}
+        }
+    }
 
-	protected String[] getHeaderFields(IMCMessage m) {
+    protected String[] getHeaderFields(IMCMessage m) {
+        if (headerFields == null) {
+            headerFields = new String[]{"timestamp", "src", "src_ent", "dst", "dst_ent"};
+            if (m.getHeader().getTypeOf("timestamp") == null)
+                headerFields[0] = "time";
+        }
+        return headerFields;
+    }
 
-	    if (headerFields == null) {
-	        headerFields = new String[] {"timestamp", "src", "src_ent", "dst", "dst_ent"};
-	        if (m.getHeader().getTypeOf("timestamp") == null)
-	            headerFields[0] = "time";
-	    }
-	    return headerFields;
-	}
+    public int logMessage(IMCMessage message) throws IOException {
+        message = message.cloneMessage();
+        int id = message.getMessageType().getId(), count = 0;
+        if (!writers.containsKey(id))
+            writeHeader(message);
+        BufferedWriter bw = writers.get(id);
+        boolean first = true;
+        message.setTimestamp(message.getTimestamp() - startTime);
 
-	public int logMessage(IMCMessage message) throws IOException {
-	    message = message.cloneMessage();
-		int id = message.getMessageType().getId(), count = 0;
-		if (!writers.containsKey(id))
-			writeHeader(message);
-		BufferedWriter bw = writers.get(id);
-		boolean first = true;
-		message.setTimestamp(message.getTimestamp()-startTime);
-
-		for (String s : getHeaderFields(message)) {
-			if (!first)
-				bw.write("\t");
-			first = false;
-			try {
-			    bw.write(message.getHeader().getAsString(s));
-			}
-			catch (Exception e) {
-			   e.printStackTrace();
+        for (String s : getHeaderFields(message)) {
+            if (!first)
+                bw.write("\t");
+            first = false;
+            try {
+                bw.write(message.getHeader().getAsString(s));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-		}
+        }
 
-		Vector<IMCMessage> innerMessages = new Vector<IMCMessage>();
+        Vector<IMCMessage> innerMessages = new Vector<>();
 
-		for (String s : message.getMessageType().getFieldNames()) {
-			if (message.getMessageType().getFieldType(s) == IMCFieldType.TYPE_MESSAGE) {
-				IMCMessage m = message.getMessage(s);
-				if (m != null)
-					innerMessages.add(m);
-			}
-			if (message.getMessageType().getFieldType(s) == IMCFieldType.TYPE_MESSAGELIST) {
+        for (String s : message.getMessageType().getFieldNames()) {
+            if (message.getMessageType().getFieldType(s) == IMCFieldType.TYPE_MESSAGE) {
+                IMCMessage m = message.getMessage(s);
+                if (m != null)
+                    innerMessages.add(m);
+            }
+            if (message.getMessageType().getFieldType(s) == IMCFieldType.TYPE_MESSAGELIST) {
                 Vector<IMCMessage> ms = message.getMessageList(s);
                 if (ms != null)
                     innerMessages.addAll(ms);
             }
-			bw.write("\t"+message.getAsString(s));
-		}
-		bw.write("\r\n");
-		count ++;
+            bw.write("\t" + message.getAsString(s));
+        }
+        bw.write("\r\n");
+        count++;
 
-		for (IMCMessage m : innerMessages) {
+        for (IMCMessage m : innerMessages) {
 
-		    for (String f : new String[] {"src", "dst", "src_ent", "dst_ent"})
-			m.getHeader().setValue(f, message.getHeader().getValue(f));
-			m.getHeader().set_timestamp(message.getTimestamp());
-		    count += logMessage(m);
-		}
+            for (String f : new String[]{"src", "dst", "src_ent", "dst_ent"})
+                m.getHeader().setValue(f, message.getHeader().getValue(f));
+            m.getHeader().set_timestamp(message.getTimestamp());
+            count += logMessage(m);
+        }
 
-		return count;
-	}
+        return count;
+    }
 
-	public void writeHeader(IMCMessage message) throws IOException {
+    public void writeHeader(IMCMessage message) throws IOException {
 
-	    if (startTime <= 0)
-	        startTime = message.getTimestamp();
-	    int id = message.getMessageType().getId();
-		writers.put(id, new BufferedWriter(new FileWriter(new File(dir, message.getMessageType().getShortName()+".llf"))));
-		BufferedWriter writer = writers.get(id);
-		writer.write("#LLF1\r\n#generator: IMCJava\r\n#standard: IMC\r\n#version: "
-				+ defs.getVersion() + "\r\n#name: "
-				+ message.getMessageType().getShortName() + "\r\n#date: " + (new Date())
-				+ "\r\n#startTime: " + startTime + "\r\n#TYPES: ");
+        if (startTime <= 0)
+            startTime = message.getTimestamp();
+        int id = message.getMessageType().getId();
+        writers.put(id, new BufferedWriter(new FileWriter(new File(dir, message.getMessageType().getShortName() + ".llf"))));
+        BufferedWriter writer = writers.get(id);
+        writer.write("#LLF1\r\n#generator: IMCJava\r\n#standard: IMC\r\n#version: "
+                + defs.getVersion() + "\r\n#name: "
+                + message.getMessageType().getShortName() + "\r\n#date: " + (new Date())
+                + "\r\n#startTime: " + startTime + "\r\n#TYPES: ");
 
-		String types = "", names = "";
+        StringBuilder types = new StringBuilder();
+        StringBuilder names = new StringBuilder();
 
-		for (String s : getHeaderFields(message)) {
-			types += message.getHeader().getTypeOf(s)+"\t";
-			if ( message.getHeader().getTypeOf(s) == null) {
-			    System.err.println("could not get type of "+s+" for message "+message.getAbbrev());
-			}
-		}
+        for (String s : getHeaderFields(message)) {
+            types.append(message.getHeader().getTypeOf(s)).append("\t");
+            if (message.getHeader().getTypeOf(s) == null) {
+                System.err.println("could not get type of " + s + " for message " + message.getAbbrev());
+            }
+        }
 
-		for (IMCFieldType t : message.getMessageType().getFieldIMCTypes())
-			types += t+"\t";
+        for (IMCFieldType t : message.getMessageType().getFieldIMCTypes())
+            types.append(t).append("\t");
 
-		for (String s : headerNames)
-			names += s+"\t";
-		for (String s : message.getMessageType().getFieldNames())
-			names += s+"\t";
+        for (String s : headerNames)
+            names.append(s).append("\t");
+        for (String s : message.getMessageType().getFieldNames())
+            names.append(s).append("\t");
 
-		writer.write(types.trim()+"\r\n");
-		writer.write(names.trim()+"\r\n");
-	}
+        writer.write(types.toString().trim() + "\r\n");
+        writer.write(names.toString().trim() + "\r\n");
+    }
 
-	public void close() {
-		for (BufferedWriter bw : writers.values()) {
-			try {
-				bw.close();
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		writers.clear();
-	}
+    public void close() {
+        for (BufferedWriter bw : writers.values()) {
+            try {
+                bw.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        writers.clear();
+    }
 }
