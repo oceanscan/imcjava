84c8d05562324de0256e9d20bec0d1013c8738c9
José Pinto <zepinto@gmail.com>
2021-12-21 16:11:58 +0000

    Added mvn_update.sh for updating maven repository.

---
+++ mvn_update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+gradle publishToMavenLocal
+version=`gradle printVersion | sed -n 3p`
+
+echo ""
+echo "Publishing ICMJava v$version to omst's Maven repository..."
+sleep 1
+
+scp -r ~/.m2/repository/pt/lsts/imcjava/$version omst@192.168.61.1:/srv/data/www/maven/pt/lsts/imcjava/
