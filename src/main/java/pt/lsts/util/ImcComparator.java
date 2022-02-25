03b14a7107809796b5694a5e345c7e9d547ad69a
José Pinto <zepinto@gmail.com>
2022-02-25 16:07:20 +0000

    Improved IMCComparator.

--- src/main/java/pt/lsts/util/ImcComparator.java
+++ src/main/java/pt/lsts/util/ImcComparator.java
@@ -1,5 +1,7 @@
 package pt.lsts.util;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 import pt.lsts.imc.IMCDefinition;
@@ -11,7 +13,7 @@ import pt.lsts.imc.IMCUtil;
  */
 public class ImcComparator {
 
-    public static void compare(IMCDefinition defs1, IMCDefinition defs2) throws Exception {
+    public static void compare(IMCDefinition defs1, String name1, IMCDefinition defs2, String name2) throws Exception {
 
         if (defs1.getSyncWord() != defs2.getSyncWord())
             throw new Exception("Synch words do not match: "+defs1.getSyncWord()+" vs "+defs2.getSyncWord());
@@ -19,32 +21,78 @@ public class ImcComparator {
         HashSet<String> checkedMessages = new HashSet<>();
 
         for (String msg : defs1.getMessageNames()) {
+            int id = defs1.getMessageId(msg);            
+            String abbrev2 = defs2.getMessageName(id); 
+            if (abbrev2 != null && !abbrev2.equals(msg)) {
+                System.err.println(msg+" id ("+id+") corresponds to a different message in "+name2+" protocol: "+abbrev2);
+            }
             IMCMessage message1 = defs1.create(msg);
             IMCUtil.fillWithRandomData(message1);
-            IMCMessage message2 = defs2.create(msg);
+            IMCMessage message2 = defs2.create(msg);            
             if (message2 == null) {
-                System.err.println(msg+" does not exists in second IMC definitions.");
+                System.err.println(msg+" does not exist in "+name2+" protocol.");
             }
             else {
                 message2.copyFrom(message1);
+                if (message2.getMgid() != message1.getMgid()) {
+                    System.err.println(msg+" has conflicting IDs.");
+                }
+                String[] json1 = message1.asJSON(true).split("\n");
+                String[] json2 = message2.asJSON(true).split("\n");
+                for (int i = 0; i < json1.length; i++) {
+                    if (!json1[i].equals(json2[i])) {
+                        //System.out.println("   "+json1[i-1]);
+                        System.out.println(" > "+json1[i]+" vs "+json2[i]);
+                        //System.out.println("   "+json1[i+1]);
+                        break;
+                    }
+                }
+
                 if (!message1.asJSON(true).equals(message2.asJSON(true)))
                     throw new Exception("Mismatched serializaton of message "+msg+".");
                 }
+                checkedMessages.add(msg);
         }
 
         for (String msg : defs2.getMessageNames()) {
+            if (checkedMessages.contains(msg))
+                continue;
+            
+            int id = defs2.getMessageId(msg);            
+            String abbrev1 = defs1.getMessageName(id); 
+            if (abbrev1 != null && !abbrev1.equals(msg)) {
+                System.err.println(msg+" id ("+id+") corresponds to a different message in "+name1+" protocol: "+abbrev1);
+            }
             IMCMessage message2 = defs2.create(msg);
-            IMCUtil.fillWithRandomData(message2);
             IMCMessage message1 = defs1.create(msg);
             if (message1 == null) {
-                System.err.println(msg+" does not exists in first IMC definitions.");
+                System.err.println(msg+" does not exists in "+name1+" protocol.");
             }
             else {
-                message2.copyFrom(message1);
+                IMCUtil.fillWithRandomData(message2);
+                message1.copyFrom(message2);
+                String[] json1 = message1.asJSON(true).split("\n");
+                String[] json2 = message2.asJSON(true).split("\n");
+                
+                for (int i = 0; i < json1.length; i++) {
+                    if (!json1[i].equals(json2[i])) {
+                        //System.out.println("   "+json1[i-1]);
+                        System.out.println(" > "+json1[i]+" vs "+json2[i]);
+                        //System.out.println("   "+json1[i+1]);
+                        break;
+                    }
+                }
                 if (!message1.asJSON(true).equals(message2.asJSON(true)))
                     throw new Exception("Mismatched serializaton of message "+msg+".");
                 }
         }
     }
-    
+
+    public static void main(String[] args) throws Exception {
+        IMCDefinition defsOMST, defsUnify;
+        defsOMST = new IMCDefinition(new File("/home/zp/workspace/IMCs/omst/IMC.xml"));
+        defsUnify = new IMCDefinition(new File("/home/zp/workspace/IMCs/lsts-unify/IMC.xml"));
+
+        ImcComparator.compare(defsOMST, "OMST",  defsUnify, "LSTS");
+    }    
 }
