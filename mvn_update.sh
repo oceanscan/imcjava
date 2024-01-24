51398bafe37107fcc4deb9bf68727e18c634f80c
Renato Campos <rcampos.oceanscan@gmail.com>
2024-01-24 13:37:49 +0000

    mvn_update: upload to NAS

--- mvn_update.sh
+++ mvn_update.sh
@@ -7,4 +7,4 @@ echo ""
 echo "Publishing ICMJava v$version to omst's Maven repository..."
 sleep 1
 
-scp -r ~/.m2/repository/pt/lsts/imcjava/$version omst@192.168.61.1:/srv/data/www/maven/pt/lsts/imcjava/
+scp -r ~/.m2/repository/pt/lsts/imcjava/$version devops@192.168.61.1:/volume1/web/software/maven/pt/lsts/imcjava/
