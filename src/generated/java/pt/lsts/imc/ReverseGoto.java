6d563bda9bc4099236638ca0497196124744c5ac
Jose Pinto <zepinto@gmail.com>
2026-04-09 13:48:48 +0100

    IMC V5.5.8.2-beta

---
+++ src/generated/java/pt/lsts/imc/ReverseGoto.java
@@ -0,0 +1,372 @@
+/*
+ * Below is the copyright agreement for IMCJava.
+ * 
+ * Copyright (c) 2010-2016, Laboratório de Sistemas e Tecnologia Subaquática
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     - Neither the names of IMC, LSTS, IMCJava nor the names of its 
+ *       contributors may be used to endorse or promote products derived from 
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL LABORATORIO DE SISTEMAS E TECNOLOGIA SUBAQUATICA
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+package pt.lsts.imc;
+
+/**
+ *  IMC Message Reverse Goto Maneuver (2046)<br/>
+ *  A "ReverseGoto" is a maneuver specifying a movement of the vehicle to a<br/>
+ *  target waypoint while moving in reverse.<br/>
+ *  This maneuver mirrors Goto semantics but speed must be strictly negative.<br/>
+ *  If speed is zero or positive, the maneuver should be rejected by the<br/>
+ *  maneuver controller.<br/>
+ */
+
+@SuppressWarnings("unchecked")
+public class ReverseGoto extends Maneuver {
+
+	public enum Z_UNITS {
+		NONE(0),
+		DEPTH(1),
+		ALTITUDE(2),
+		HEIGHT(3);
+
+		protected long value;
+
+		public long value() {
+			return value;
+		}
+
+		Z_UNITS(long value) {
+			this.value = value;
+		}
+	}
+
+	public enum SPEED_UNITS {
+		METERS_PS(0),
+		RPM(1),
+		PERCENTAGE(2);
+
+		protected long value;
+
+		public long value() {
+			return value;
+		}
+
+		SPEED_UNITS(long value) {
+			this.value = value;
+		}
+	}
+
+	public static final int ID_STATIC = 2046;
+
+	public ReverseGoto() {
+		super(ID_STATIC);
+	}
+
+	public ReverseGoto(IMCMessage msg) {
+		super(ID_STATIC);
+		try{
+			copyFrom(msg);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ReverseGoto(IMCDefinition defs) {
+		super(defs, ID_STATIC);
+	}
+
+	public ReverseGoto(IMCDefinition defs, int type) {
+		super(defs, type);
+	}
+
+	public static ReverseGoto create(Object... values) {
+		ReverseGoto m = new ReverseGoto();
+		for (int i = 0; i < values.length-1; i+= 2)
+			m.setValue(values[i].toString(), values[i+1]);
+		return m;
+	}
+
+	public static ReverseGoto clone(IMCMessage msg) throws Exception {
+
+		ReverseGoto m = new ReverseGoto();
+		if (msg == null)
+			return m;
+		if(msg.definitions != m.definitions){
+			msg = msg.cloneMessage();
+			IMCUtil.updateMessage(msg, m.definitions);
+		}
+		else if (msg.getMgid()!=m.getMgid())
+			throw new Exception("Argument "+msg.getAbbrev()+" is incompatible with message "+m.getAbbrev());
+
+		m.getHeader().values.putAll(msg.getHeader().values);
+		m.values.putAll(msg.values);
+		return m;
+	}
+
+	public ReverseGoto(int timeout, double lat, double lon, float z, Z_UNITS z_units, float speed, SPEED_UNITS speed_units, double roll, double pitch, double yaw, String custom) {
+		super(ID_STATIC);
+		setTimeout(timeout);
+		setLat(lat);
+		setLon(lon);
+		setZ(z);
+		setZUnits(z_units);
+		setSpeed(speed);
+		setSpeedUnits(speed_units);
+		setRoll(roll);
+		setPitch(pitch);
+		setYaw(yaw);
+		if (custom != null)
+			setCustom(custom);
+	}
+
+	/**
+	 *  @return Timeout (s) - uint16_t
+	 */
+	public int getTimeout() {
+		return getInteger("timeout");
+	}
+
+	/**
+	 *  @param timeout Timeout (s)
+	 */
+	public ReverseGoto setTimeout(int timeout) {
+		values.put("timeout", timeout);
+		return this;
+	}
+
+	/**
+	 *  @return Latitude WGS-84 (rad) - fp64_t
+	 */
+	public double getLat() {
+		return getDouble("lat");
+	}
+
+	/**
+	 *  @param lat Latitude WGS-84 (rad)
+	 */
+	public ReverseGoto setLat(double lat) {
+		values.put("lat", lat);
+		return this;
+	}
+
+	/**
+	 *  @return Longitude WGS-84 (rad) - fp64_t
+	 */
+	public double getLon() {
+		return getDouble("lon");
+	}
+
+	/**
+	 *  @param lon Longitude WGS-84 (rad)
+	 */
+	public ReverseGoto setLon(double lon) {
+		values.put("lon", lon);
+		return this;
+	}
+
+	/**
+	 *  @return Z Reference (m) - fp32_t
+	 */
+	public double getZ() {
+		return getDouble("z");
+	}
+
+	/**
+	 *  @param z Z Reference (m)
+	 */
+	public ReverseGoto setZ(double z) {
+		values.put("z", z);
+		return this;
+	}
+
+	/**
+	 *  @return Z Units (enumerated) - uint8_t
+	 */
+	public Z_UNITS getZUnits() {
+		try {
+			Z_UNITS o = Z_UNITS.valueOf(getMessageType().getFieldPossibleValues("z_units").get(getLong("z_units")));
+			return o;
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getZUnitsStr() {
+		return getString("z_units");
+	}
+
+	public short getZUnitsVal() {
+		return (short) getInteger("z_units");
+	}
+
+	/**
+	 *  @param z_units Z Units (enumerated)
+	 */
+	public ReverseGoto setZUnits(Z_UNITS z_units) {
+		values.put("z_units", z_units.value());
+		return this;
+	}
+
+	/**
+	 *  @param z_units Z Units (as a String)
+	 */
+	public ReverseGoto setZUnitsStr(String z_units) {
+		setValue("z_units", z_units);
+		return this;
+	}
+
+	/**
+	 *  @param z_units Z Units (integer value)
+	 */
+	public ReverseGoto setZUnitsVal(short z_units) {
+		setValue("z_units", z_units);
+		return this;
+	}
+
+	/**
+	 *  @return Speed - fp32_t
+	 */
+	public double getSpeed() {
+		return getDouble("speed");
+	}
+
+	/**
+	 *  @param speed Speed
+	 */
+	public ReverseGoto setSpeed(double speed) {
+		values.put("speed", speed);
+		return this;
+	}
+
+	/**
+	 *  @return Speed Units (enumerated) - uint8_t
+	 */
+	public SPEED_UNITS getSpeedUnits() {
+		try {
+			SPEED_UNITS o = SPEED_UNITS.valueOf(getMessageType().getFieldPossibleValues("speed_units").get(getLong("speed_units")));
+			return o;
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getSpeedUnitsStr() {
+		return getString("speed_units");
+	}
+
+	public short getSpeedUnitsVal() {
+		return (short) getInteger("speed_units");
+	}
+
+	/**
+	 *  @param speed_units Speed Units (enumerated)
+	 */
+	public ReverseGoto setSpeedUnits(SPEED_UNITS speed_units) {
+		values.put("speed_units", speed_units.value());
+		return this;
+	}
+
+	/**
+	 *  @param speed_units Speed Units (as a String)
+	 */
+	public ReverseGoto setSpeedUnitsStr(String speed_units) {
+		setValue("speed_units", speed_units);
+		return this;
+	}
+
+	/**
+	 *  @param speed_units Speed Units (integer value)
+	 */
+	public ReverseGoto setSpeedUnitsVal(short speed_units) {
+		setValue("speed_units", speed_units);
+		return this;
+	}
+
+	/**
+	 *  @return Roll (rad) - fp64_t
+	 */
+	public double getRoll() {
+		return getDouble("roll");
+	}
+
+	/**
+	 *  @param roll Roll (rad)
+	 */
+	public ReverseGoto setRoll(double roll) {
+		values.put("roll", roll);
+		return this;
+	}
+
+	/**
+	 *  @return Pitch (rad) - fp64_t
+	 */
+	public double getPitch() {
+		return getDouble("pitch");
+	}
+
+	/**
+	 *  @param pitch Pitch (rad)
+	 */
+	public ReverseGoto setPitch(double pitch) {
+		values.put("pitch", pitch);
+		return this;
+	}
+
+	/**
+	 *  @return Yaw (rad) - fp64_t
+	 */
+	public double getYaw() {
+		return getDouble("yaw");
+	}
+
+	/**
+	 *  @param yaw Yaw (rad)
+	 */
+	public ReverseGoto setYaw(double yaw) {
+		values.put("yaw", yaw);
+		return this;
+	}
+
+	/**
+	 *  @return Custom settings for maneuver (tuplelist) - plaintext
+	 */
+	public java.util.LinkedHashMap<String, String> getCustom() {
+		return getTupleList("custom");
+	}
+
+	/**
+	 *  @param custom Custom settings for maneuver (tuplelist)
+	 */
+	public ReverseGoto setCustom(java.util.LinkedHashMap<String, ?> custom) {
+		String val = encodeTupleList(custom);
+		values.put("custom", val);
+		return this;
+	}
+
+	public ReverseGoto setCustom(String custom) {
+		values.put("custom", custom);
+		return this;
+	}
+
+}
